6c5c0f65d6965988d392dab93b26bd4eb20a4258
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-09-18 14:19:52 +0530

    mlflow tracking done

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     AdaBoostClassifier
 )
+import mlflow
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,
                  data_transformation_artifact:DataTransformationArtifact):
@@ -31,6 +32,16 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e, sys) from e
         
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1 score", f1_score)
+            mlflow.log_metric("precision score", precision_score)
+            mlflow.log_metric("recall score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
 
     def train_model(self,x_train,y_train,x_test,y_test):
         model={
@@ -91,12 +102,13 @@ class ModelTrainer:
         
         classification_train_metric= get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
-        ##track the mlflow
-        #def track(sekd):
+        ##track the the exp with mlflow
+        self.track_mlflow(best_model,classification_train_metric)
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test, y_pred=y_test_pred)
 
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor=load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path=os.path.dirname(self.model_trainer_config.trained_model_file_path)
