8739ac77938fcb0cc92ea7ec2343a0649c4f0ba2
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-09-18 14:46:36 +0530

    model pushed-Local

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -120,8 +120,9 @@ class ModelTrainer:
         os.makedirs(model_dir_path,exist_ok=True)
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
-        save_object(self.model_trainer_config.trained_model_file_path,obj= Network_Model)
-
+        save_object(self.model_trainer_config.trained_model_file_path,obj= NetworkModel)
+        
+        save_object("final_models/model.pkl",best_model)
 
         #Model trainer artifact
         model_trainer_artifact=ModelTrainerArtifact(
